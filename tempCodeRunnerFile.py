3f407c0359ec4ee6b95eebefe032700251522ae4
Anas Elsaba <anaselsaba123@gmail.com>
2025-06-21 11:38:32 +0300

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# Allow main window and frame to resize properly
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
